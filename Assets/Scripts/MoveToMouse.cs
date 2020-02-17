73a6f76228af59ee722158e62085aa4716048d49
aboveya <tinklerc@gmail.com>
2020-02-16 21:24:02 -0500

    Movement turned to button based movement

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveToMouse : MonoBehaviour
 {
+
+  public Button taskButton;
   public GameObject treeButton;
   public float speed = 5;
   bool action = false;
@@ -11,51 +14,75 @@ public class MoveToMouse : MonoBehaviour
 
   private GameObject trackedObject;
   private Vector2 target;
+
+  bool uiActive = false;
+  bool buttonWasPressed = false;
+  
   
   void Start () {
-    treeButton.SetActive (false);
-
+		taskButton.onClick.AddListener(onTaskClick);
+    treeButton.SetActive (uiActive);
   }
 
-  void MoveObjects() {
-    if(this.trackedObject == null || this.target == null) { return; }
-    var currentPos = this.trackedObject.transform.position;
-    this.trackedObject.transform.position = Vector2.MoveTowards(currentPos, target, speed * Time.deltaTime);
+  void onTaskClick() {
+    Debug.Log("ya clicked me!");
+    this.buttonWasPressed = true;
+    this.uiActive = false;
+    treeButton.SetActive(this.uiActive);
+  }
+
+
+  void HandleLeftClick() {
+    if(!Input.GetMouseButtonDown(0)) { return; }
+    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    var t = SelectTarget();
+    if(t != null) {
+      trackedObject = t;
+      this.uiActive = true;
+      treeButton.SetActive(uiActive);
+    }
+
+    if(this.buttonWasPressed) {
+      trackedObject.GetComponent<Tree>().Target = mousePos;
+      this.buttonWasPressed = false;
+    }
+  }
+  GameObject SelectTarget() {
+    GameObject result = null;
+    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+    if (hit.collider != null && hit.collider.gameObject != null) {
+      var hitObject = hit.collider.gameObject;
+      if(hitObject.name == "Tree(Clone)") {
+        result = hitObject;
+      }
+    }
+    return result;
   }
   
   void Update () {
 
-    MoveObjects();
+    HandleLeftClick();
 
-    if (this.trackedObject != null && Input.GetMouseButtonDown(0)) {
-      var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      this.target = new Vector2(mouse.x, mouse.y);
-      
-    }
-      
-    if (Input.GetMouseButtonDown(1)) {
-      this.trackedObject = null;
-      treeButton.SetActive (false);
-    }
 
-    if (Input.GetMouseButtonDown(0)) {
-      Debug.Log("click");
-      Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-      Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+    // treeButton.SetActive (true);
+    // if (this.trackedObject != null && Input.GetMouseButtonDown(0)) {
+    //   var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //   this.target = new Vector2(mouse.x, mouse.y);
       
-      RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-      if (hit.collider != null && hit.collider.gameObject != null) {
-        var hitObject = hit.collider.gameObject;
-        if(hitObject.name == "Tree(Clone)") {
-          this.trackedObject = hitObject;
-          treeButton.SetActive (true);
-          Debug.Log("tracking - " + this.trackedObject.name);
-        } else {
-          Debug.Log("Expected tree but did not get");
-          Debug.Log("Got " + hitObject.name);
-        }
-      }
-    }
+    // }
+      
+    // if (Input.GetMouseButtonDown(1)) {
+    //   this.trackedObject = null;
+    //   treeButton.SetActive (false);
+    // }
+
+    // if (Input.GetMouseButtonDown(0)) {
+    //   Debug.Log("click");
+    
+     
+    // }
   }    
  
 }
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public Vector2 Target;
+    public float Speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    void MoveToTarget() 
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Target, Speed * Time.deltaTime);
+    }
+}
